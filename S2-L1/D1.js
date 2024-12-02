e492606b2656f76c3a420314619ead43a48d67b5
Andrea Zimbardo <andreazimbardo89@gmail.com>
2024-12-02 17:14:25 +0100

    JS S2-L1

--- S2-L1/D1.js
+++ S2-L1/D1.js
@@ -103,5 +103,6 @@ console.log(sottrazione);
 let name1="john";
 let name2="John";
 console.log(name1 === name2);
+console.log(name1 === name2.toLocaleLowerCase());
 
 /* SCRIVI QUI LA TUA RISPOSTA */
